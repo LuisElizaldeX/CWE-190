8ca5f2cebdac3d8a959b298db1b995bc0ed25247
Luis <131182504+LuisElizaldeX@users.noreply.github.com>
2024-03-17 22:47:40 -0600

    Add files via upload

---
+++ programa.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+
+int main(){
+    int i;
+    unsigned int j;
+
+    printf("Ejemplo 1\n");
+    i=__INT_MAX__;
+    printf("i =  %d\n", i);
+    i++;
+    printf("i = %d\n", i);
+
+    j=__UINT32_MAX__;
+    printf("j = %u\n", j);
+    j++;
+    printf("j = %u\n", j);
+
+    printf("\nEjemplo 2\n");
+    i = 1;
+    unsigned short u;
+    u = i;
+    printf("i = %d\n", i);
+    printf("u = %d\n", u);
+
+    printf("\nEjemplo 3\n");
+    char cresult, c1, c2;
+    c1 = 100;
+    c2 = 27;
+    cresult = c1 + c2;
+    printf("c = %d\n", cresult);
+    c1 = 100;
+    c2 = 28;
+    cresult = c1 + c2;
+    printf("c = %d\n", cresult);
+}
